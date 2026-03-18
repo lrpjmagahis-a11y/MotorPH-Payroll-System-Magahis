7ecec28cca088b0675c51707d0ed71169bbd017c
Princess Jade Magahis <lr.pjmagahis@mmdc.mcl.edu.ph>
2026-03-18 13:03:27 +0800

    Create MotorPH_System.java

---
+++ MotorPH_System.java
@@ -0,0 +1,7 @@
+public class MotorPH_System {
+    public static void main(String[] args) {
+        System.out.println("MotorPH Payroll System v2.0");
+        System.out.println("Loading 100 employee records...");
+        System.out.println("System Ready.");
+    }
+}
